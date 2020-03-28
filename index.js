fe414233045266e311d2afa8abb0626ef02d1c3c
serin <oussamakatalonya1@gmail.com>
2020-03-28 19:32:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ fs.readdir('./commands/', (err, files) => {
 
 client.on('ready', () => {
   setInterval(() => {
-    client.user.setPresence({ game: { name: `Under Developing`, type: 1, url: "https://www.twitch.tv/nightcore_at" }})
+    client.user.setPresence({ game: { name: `Nothing`, type: 1, url: "https://www.twitch.tv/" }})
   }, 60000);
 });
 

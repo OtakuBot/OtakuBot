d75cb51a974e8dba6e16313ddcaad6c0ca03b551
serin <oussamakatalonya1@gmail.com>
2020-03-29 20:15:48 +0100

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,45 @@
+const Discord = require('discord.js')
+const help = require('../data/helpMsgs.json');
+const customisation = require('../customisation.json');
+const settings = require('../settings.json');
+const fs = require('fs')
+exports.run = (client, message, args) => {
+  if (!message.channel.nsfw) return message.channel.send("حبس كيما راك")
+  if(!args[0]){
+      message.author.send("***Moderation***\n" + `${help.helpMsg1}` + "\n" + "***Usefull***\n" + `${help.helpMsg2}`)
+      message.author.send("***Fun***\n"+`${help.helpMsg3}`+"***Action***\n"+`${help.helpMsg4}`)
+	  
+      .catch(e =>{
+        if (e) {
+        message.channel.send(`Error. You seems to be locking your DMs so I'll send it here instead.`);
+        message.channel.send("***Moderation***\n" + `${help.helpMsg1}` + "\n" + "***Usefull***\n" + `${help.helpMsg2}`)
+        message.channel.send("***Fun***\n"+`${help.helpMsg3}`+"***Action***\n"+`${help.helpMsg4}`)
+		
+      }
+      });
+    }else{
+      let command = args[0];
+      if (client.commands.has(command)) {
+        cmd = client.commands.get(command);
+        return message.channel.send("```" + `\n` + "Description: " + cmd.help.description + `\n` + "Usage: " + cmd.help.usage + "```")
+      } else if (client.aliases.has(command)) {
+        cmd = client.commands.get(client.aliases.get(command));
+        return message.channel.send("```" + `\n` + "Description: " + cmd.help.description + `\n` + "Usage: " + cmd.help.usage + "```")
+      } else {
+        return message.reply("That command doesn't exist!")
+      }
+    }
+  }
+
+exports.conf = {
+  enabled: false,
+  guildOnly: false,
+  aliases: ['h', 'halp'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'help',
+  description: 'Displays all the available commands for your permission level.',
+  usage: 'help [command]'
+};

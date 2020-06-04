a38a306a3eeb82de576fa13cb02c06c5f961f60a
serin <oussamakatalonya1@gmail.com>
2020-06-04 19:11:49 +0100

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,19 +1,21 @@
-exports.run = (client, message) => {
-    let args = message.content.split(" ").slice(1);
-    message.delete();
-    if (args.join(" ") === "@everyone" || args.join(" ") === "@here") return message.channel.send("You ain't making me Ping anyone BOI!");
-    message.channel.send(args.join(" "));
+//By NightcoreAT#3678
+exports.run = (client, message) => { //from here your script is started
+    let args = message.content.split(" ").slice(1); // let's check about your args and msg
+    message.delete(); //The bot will delete the message you wrote , The message will become the bot which write it and not you : For bots, it requires permission MANAGE_MESSAGE
+    if (args.join(" ") === "@everyone" || args.join(" ") === "@here") return message.channel.send("You ain't making me Ping anyone BOI!"); // Let’s see what’s going on if you want everyone
+    message.channel.send(args.join(" ")); // What you write will be shown as bot 
 };
 
 exports.conf = {
     enabled: true,
     guildOnly: false,
-    aliases: [],
+    aliases: [], // leave it empty
     permLevel: 0
 };
 
 exports.help = {
-    name: "say",
-    description: "Makes the bot repeat your message.",
-    usage: "say [message]"
+    name: "say", //your command name
+    description: "Makes the bot repeat your message.", //your command description
+    usage: "say [message]" //how did it work : say [hi, how are you] as example !
 };
+//By NightcoreAT#3678

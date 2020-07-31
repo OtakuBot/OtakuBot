8e1e9a49a6bf2dff022597510ef54b130d5599e3
serin <oussamakatalonya1@gmail.com>
2020-07-31 09:41:03 +0100

    Update smack.js

--- commands/smack.js
+++ commands/smack.js
@@ -1,9 +1,9 @@
-exports.run = (client, message, args) => {
-  let user = message.mentions.users.first();
+exports.run = (client, message, args) => { //lets started your commands script
+  let user = message.mentions.users.first(); //lets check that which mentions first well not be as smacked
   let reason = args.slice(0).join(' ');
-  if (reason.length < 1) return message.reply('You can\'t smack thin air, pick someone fool.');
-  if(message.mentions.users.first().id === "242263403001937920") return message.reply('You can\'t hurt him you pleblord.:facepalm:');
-  message.channel.send(`${message.author.username} smacked ${message.mentions.users.first().username}.`)
+  if (reason.length < 1) return message.reply('You cant smack thin air, pick someone fool.'); //lets make a reply if no one mentions
+  if(message.mentions.users.first().id === "SetYourID") return message.reply('you cant hurt them , he well kill you.:facepalm:'); // lets make a some fun reply
+  message.channel.send(`${message.author.username} smacked ${message.mentions.users.first().username}.`) //lets make a some fun reply
   }
   
 exports.conf = {
@@ -13,8 +13,9 @@ exports.conf = {
   permLevel: 0
 };
 
-exports.help = {
-  name: 'smack',
-  description: 'Smacks a user.',
-  usage: 'smack <user>'
+exports.help = { //lets load your commands
+  name: 'smack', // commands name
+  description: 'Smacks a user.', //commands description
+  usage: 'smack <user>' //how they work
 };
+//By NightcoreAT#3678

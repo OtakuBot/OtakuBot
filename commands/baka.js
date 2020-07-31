46560a4b796e742dc7373fba3f4770617a3cc2ba
serin <oussamakatalonya1@gmail.com>
2020-07-31 08:36:15 +0100

    Update baka.js

--- commands/baka.js
+++ commands/baka.js
@@ -1,15 +1,15 @@
-const Discord = require('discord.js');
-const superagent = require('superagent');
+const Discord = require('discord.js');//npm i discordjs@12.2.0 
+const superagent = require('superagent'); //npm i superagent
 const customisation = require('../customisation.json');
 
 exports.run = async (client, message, args, tools) => {
     const { body } = await superagent
-    .get("https://nekos.life/api/v2/img/baka");
+    .get("https://nekos.life/api/v2/img/baka"); //wut we went to use
     
-    const embed = new Discord.RichEmbed()
+    const embed = new Discord.MessageEmbed() // changed from RichEmbed to MessageEmbed Method
     .setColor("#ff9900")
-    .setImage(body.url) 
-    .setFooter(`© NightcoreAMV Bot by ${customisation.ownername}`);
+    .setImage(body.url)  // your commands well showing your order here
+    .setFooter(`© ${customisation.ownername}`);
     message.channel.send({embed})
 };
 
@@ -25,3 +25,4 @@ exports.conf = {
     description: 'Baka UwU',
     usage: 'baka'
   };
+//By NightcoreAT#3678

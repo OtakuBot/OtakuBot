e2841f7eb634cc349f1aff1cc806f793aacae61e
serin <oussamakatalonya1@gmail.com>
2020-07-31 10:29:29 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
-exports.run = (client, message) => {
-  message.channel.send('Ping?').then(m => m.edit(`Roundtrip took: ${message.createdTimestamp - m.createdTimestamp}ms. Heartbeat: ${Math.round(client.ping)}ms.`))
-};
+exports.run = (client, message) => { //here your commands started from
+  message.channel.send('Ping?').then(m => m.edit(`Roundtrip took: ${message.createdTimestamp - m.createdTimestamp}ms. Heartbeat: ${Math.round(client.ws.ping)}ms.`))
+}; //lets show your bot ping is ...
 
 exports.conf = {
   enabled: true,
@@ -9,8 +9,9 @@ exports.conf = {
   permLevel: 0
 };
 
-exports.help = {
-  name: 'ping',
-  description: 'Ping/Pong command. I wonder what this does? /sarcasm',
-  usage: 'ping'
+exports.help = { //lets load commands 
+  name: 'ping', //commands name
+  description: 'the ping of bot', //commands description
+  usage: 'ping'//how they working
 };
+//By NightcoreAT#3678

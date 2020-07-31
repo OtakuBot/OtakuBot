3076241e30ef66989541415354ebfb786046bfc6
serin <oussamakatalonya1@gmail.com>
2020-07-31 09:03:31 +0100

    Update truth.js

--- commands/truth.js
+++ commands/truth.js
@@ -1,24 +1,27 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js'); //npm i discord.js
 const customisation = require('../customisation.json');
 
 exports.run = async (client, message, args) => {
-    if (!args[0]) return message.reply('Provide a truth!')
-    const embed = new Discord.RichEmbed()
-    .setColor("#ff9900")
-    .setImage(`https://api.alexflipnote.dev/scroll?text=` + args.join('%20')) 
-    .setFooter(`©  ${customisation.ownername}`);
+    if (!args[0]) return message.reply('Provide a truth or mention as you went first thing!') // if they not mentioned nobody , so return as 'Provide a truth or mentions ....'
+    const embed = new Discord.MessageEmbed() //once discordjs is update , RichEmbed Method is rename to MessageEmbed
+    .setTitle("----") //its free you change | ${mentions.author.first} is allowed to set
+    .setColor("#ffff00") // you can set it random
+    .setImage(`https://api.alexflipnote.dev/scroll?text=` + args.join('%20')) //lets get the image which needed
+    .setFooter(`©  ${customisation.ownername}`); // set your personnel footer
     message.channel.send({embed});
 };
 
 exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: [],
+    enabled: true, // if enabled or no
+    guildOnly: false, 
+    aliases: [], // if you need aliases ? here example ['tru'] , this is work
     permLevel: 0
   };
   
-  exports.help = {
-    name: 'truth',
-    description: 'Sends a scroll truth',
-    usage: 'truth (truth)'
+  exports.help = { // let load commands
+    name: 'truth', // commands name
+    description: 'Sends a scroll truth', //commands description
+    usage: 'truth (truth)' // how use it
+    example: 'truth <@mention> | (truth)' //show how they work   
   };
+//By NightcoreAT#3678

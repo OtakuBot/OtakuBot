8f17f7bc5cba42520202d844dc53e88f69d5cefa
serin <oussamakatalonya1@gmail.com>
2020-07-31 10:16:47 +0100

    Update neko.js

--- commands/neko.js
+++ commands/neko.js
@@ -1,17 +1,17 @@
-const Discord = require('discord.js');
-const superagent = require('superagent');
-const customisation = require('../customisation.json');
+const Discord = require('discord.js'); //npm i discord.js
+const superagent = require('superagent'); //npm i superagent
+const customisation = require('../customisation.json'); 
 
-exports.run = async (client, message, args, tools) => {
+exports.run = async (client, message, args, tools) => { //lets started your commands script
     const { body } = await superagent
-    .get("https://nekos.life/api/neko");
+    .get("https://nekos.life/api/neko"); //lets see wut we went
     link = body.neko;
     
-    const embed = new Discord.RichEmbed()
-    .setColor("#ff9900")
-    .setTitle("Here's Your Neko OwO")
-    .setImage(body.neko) 
-    .setFooter(`© ${customisation.ownername}`);
+    const embed = new Discord.MessageEmbed() //onec Discordjs is updated to 12.2.0 , richembed is removed ! they replaced now as MessageEmbed
+    .setColor("#3bb9ff") //you can set it as you went
+    .setTitle("Your Neko is here !")
+    .setImage(body.neko) //here Neko is showing image
+    .setFooter(`© ${customisation.ownername}`); //your personnel Footer
     message.channel.send({embed})
 };
 
@@ -22,8 +22,9 @@ exports.conf = {
     permLevel: 0
   };
   
-  exports.help = {
-    name: 'neko',
-    description: 'Sends a random Neko OwO',
-    usage: 'neko'
+  exports.help = { //lets load commands 
+    name: 'neko', //commands name
+    description: 'Sends a random Neko ', //commands discription
+    usage: 'neko' //how they work
   };
+//By NightcoreAT#3678

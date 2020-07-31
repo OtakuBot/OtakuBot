6158f3753f42e2f564851aa5902b95ed3eba3e3a
serin <oussamakatalonya1@gmail.com>
2020-07-31 10:25:16 +0100

    Update kitsune.js

--- commands/kitsune.js
+++ commands/kitsune.js
@@ -1,16 +1,16 @@
-const Discord = require('discord.js');
-const superagent = require('superagent');
+const Discord = require('discord.js'); //npm i discord.js
+const superagent = require('superagent'); //npm i superagent
 const customisation = require('../customisation.json');
 
-exports.run = async (client, message, args, tools) => {
+exports.run = async (client, message, args, tools) => { //lets started your commands script
     const { body } = await superagent
-    .get("https://nekos.life/api/v2/img/fox_girl");
+    .get("https://nekos.life/api/v2/img/fox_girl"); //lets see wut we went
     
-    const embed = new Discord.RichEmbed()
-    .setColor("#ff9900")
-    .setTitle(`OwO, Here's your Fox Girl`)
-    .setImage(body.url) 
-    .setFooter(`© ${customisation.ownername}`);
+    const embed = new Discord.MessageEmbed() //onec Discordjs is updated to 12.2.0 , richembed is removed ! they replaced now as MessageEmbed
+    .setColor("#ff9900") //you can set it as you went
+    .setTitle(`kitsune is here !`) // optionel !
+    .setImage(body.url) //fox girl well showing here
+    .setFooter(`© ${customisation.ownername}`); //your personnel Footer
     message.channel.send({embed})
 };
 
@@ -22,7 +22,8 @@ exports.conf = {
   };
   
   exports.help = {
-    name: 'kitsune',
-    description: 'Kitsunes OwO',
-    usage: 'kitsune'
+    name: 'kitsune', //commands name
+    description: 'Kitsunes )w)', //commands description
+    usage: 'kitsune' //how they work
   };
+//By NightcoreAT#3678

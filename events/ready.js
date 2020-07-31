20ab0b21164da5a9b52192c493f02ccf3c7b2b90
serin <oussamakatalonya1@gmail.com>
2020-07-31 10:39:45 +0100

    add cache btw guilds

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,10 @@
-const chalk = require('chalk');
-const Discord = require('discord.js');
-const settings = require('../settings.json');
+const chalk = require('chalk'); //npm i clalk
+const Discord = require('discord.js'); //npm i discord.js
+const settings = require('../settings.json'); //where the bot well insert token
 const client = new Discord.Client();
-const fs = require('fs');
+const fs = require('fs'); //npm i fs
 module.exports = client => {
-    console.log(chalk.bgGreen.black(`Online and ready to serve ${client.guilds.size} servers.`));
+    console.log(chalk.bgGreen.black(`Your Bot is online with ${client.guilds.cache.size} servers.`)); //after discordjs is updated , you must add cache btw guilds.size for like i do
   let blacklist = JSON.parse(fs.readFileSync("./blacklist.json", "utf8"));
     client.guilds.forEach((guild) => {
       if (!blacklist[guild.ownerID]) {

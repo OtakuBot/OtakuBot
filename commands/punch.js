cf55b6aba7895cf76b57af91e8a3c20602e2cef5
serin <oussamakatalonya1@gmail.com>
2020-07-31 09:51:42 +0100

    Update punch.js

--- commands/punch.js
+++ commands/punch.js
@@ -1,11 +1,11 @@
-exports.run = (client, message) => {
-  var owner = "242263403001937920"
+exports.run = (client, message) => { //lets started your commands script
+  var owner = "YourID" //set your id here to never get punched
   let user = message.mentions.users.first();
-  if (message.mentions.users.size < 1) return message.reply('You must mention someone to punch them.')
+  if (message.mentions.users.size < 1) return message.reply('wait bro , You must mention someone to punch them.') // if no one is mentioned , lets reply as
         if(user.id === owner){
-          return message.reply("You can't hurt him you pleblord.");
+          return message.reply("You can't hurt him you pleblord."); //if they mentioned you and you owner , lets reply as
   }else{
-          message.reply('You have punched <@' + user.id + '>')
+          message.reply('You have punched <@' + user.id + '>') // if you mentioned , lets reply as
   }
 };
 
@@ -16,8 +16,10 @@ exports.conf = {
   permLevel: 0
 };
 
-exports.help = {
-  name: 'punch',
-  description: 'Punches a user.',
-  usage: 'punch <user>'
+exports.help = { //lets load your commands 
+  name: 'punch', //commands name
+  description: 'Punches a user.', //commands description
+  usage: 'punch <user>', //how they work
+  example: 'punch <@Otaku#---->' //lets show a some example about how they work
 };
+//By NightcoreAT#3678

34aeb63e4d614f514d20aeac32d24ddb5c98fedd
serin <oussamakatalonya1@gmail.com>
2020-07-31 08:38:55 +0100

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -1,16 +1,16 @@
-const Discord = require('discord.js');
-const superagent = require('superagent');
-const sf = require("snekfetch");
+const Discord = require('discord.js'); //npm i discordjs@12.2.0
+const superagent = require('superagent'); //npm i superagent
+const sf = require("snekfetch"); //npm i snekfetch
 const customisation = require('../customisation.json');
 
 exports.run = async (client, message, args) => {
     const { body } = await superagent
     .get("http://aws.random.cat/meow");
 
-    const embed = new Discord.RichEmbed()
+    const embed = new Discord.MessageEmbed() //changed from RichEmbed to MessageEmbed once discord is updated to 12.0.0
     .setColor("#ff9900")
-    .setTitle("Here's Your Cat")
-    .setImage(body.file) 
+    .setTitle("Here's Your Cat") //you can change it 
+    .setImage(body.file) //here your order is showing as body
     .setFooter(`© ${customisation.ownername}`);
     message.channel.send({embed});
 };
@@ -27,3 +27,4 @@ exports.conf = {
     description: 'Sends a random cat',
     usage: 'cat'
   };
+//By NightcoreAT#3678

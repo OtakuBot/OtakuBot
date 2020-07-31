a4caada84ba8dd28ad759d191a9ba771f5fe21d7
serin <oussamakatalonya1@gmail.com>
2020-07-31 09:11:40 +0100

    Update tickle.js

--- commands/tickle.js
+++ commands/tickle.js
@@ -1,17 +1,17 @@
-const Discord = require('discord.js');
-const superagent = require('superagent');
-const customisation = require('../customisation.json');
+const Discord = require('discord.js'); //npm i discord.js
+const superagent = require('superagent'); //npm i superagent
+const customisation = require('../customisation.json'); //set your personnel footer
 
-exports.run = async (client, message, args, tools) => {
+exports.run = async (client, message, args, tools) => { // lets started your script
     if (!message.mentions.users.first()) return message.reply("You need to mention someone to tickle them");
-    if(message.mentions.users.first().id === "242263403001937920") return message.reply('You can\'t tickle him you. He will explode on impact XD.');
+    if(message.mentions.users.first().id === "you ID here") return message.reply('You cant tickle him. he well return it.');
     const { body } = await superagent
-    .get("https://nekos.life/api/v2/img/tickle");
+    .get("https://nekos.life/api/v2/img/tickle"); // lets load (body.url) image
     
-    const embed = new Discord.RichEmbed()
-    .setColor("#ff9900")
-    .setTitle(`OwO, ${message.author.username} tickled ${message.mentions.users.first().username}`)
-    .setImage(body.url) 
+    const embed = new Discord.MessageEmbed() //onec discord js is updated to newest ver , RichEmbed method is renamed to MessageEmbed
+    .setColor("#ff0000") //set is as you went
+    .setTitle(`hehe, ${message.author.username} tickled ${message.mentions.users.first().username}`) // lets make it so fun
+    .setImage(body.url) //lets show your image
     .setFooter(`© ${customisation.ownername}`);
     message.channel.send({embed})
 };
@@ -23,8 +23,10 @@ exports.conf = {
     permLevel: 0
   };
   
-  exports.help = {
-    name: 'tickle',
-    description: 'Tickles someone OwO',
-    usage: 'tickle'
+  exports.help = { 
+    name: 'tickle', // command name
+    description: 'lets tickle anyone', //commands description
+    usage: 'tickle <@mention>', //how they work
+    example: 'tickle <@Otaku#---->' //how you can use it for example
   };
+//By NightcoreAT#3678

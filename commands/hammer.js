4d01dc8855cb67d44a1a52b9eaddc3af6251b26f
serin <oussamakatalonya1@gmail.com>
2021-03-22 23:27:50 +0100

    Update to v3.2.0

--- commands/hammer.js
+++ commands/hammer.js
@@ -1,9 +1,10 @@
+//By NightcoreATDZO#3550
 const settings = require('../settings.json');
 exports.run = (client, message, args) => {
   let user = message.mentions.users.first();
   let reason = args.slice(0).join(' ');
   if (reason.length < 1) return message.reply('You can\'t throw a hammer at thin air, pick someone fool.');
-  if(message.mentions.users.first().id === "242263403001937920") return message.reply('You can\'t hurt him you pleblord.:facepalm:')
+  if(message.mentions.users.first().id === "145608354495070208") return message.reply('You can\'t hurt him you pleblord.:facepalm:')
   message.channel.send(`${message.author.username} threw a hammer at ${message.mentions.users.first().username}. :hammer:`)
   }
 

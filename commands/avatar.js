6cd57038e4635a9db192bb2e1f0bc293b6b84055
serin <oussamakatalonya1@gmail.com>
2021-03-22 23:23:05 +0100

    v3.2.0

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,20 +1,20 @@
 const Discord =  require('discord.js');
 const customisation = require('../customisation.json');
 exports.run = (client, message, args) => {
-    let avatar = message.mentions.users.size ? message.mentions.users.first().avatarURL : message.author.avatarURL;
+    let avatar = message.mentions.users.size ? message.mentions.users.first().avatarURL({ format: 'png', dynamic: true, size: 2048 }) : message.author.avatarURL({ format: 'png', dynamic: true, size: 2048 });
     if (message.mentions.users.size > 0) {
-      const embed = new Discord.RichEmbed()
+      const embed = new Discord.MessageEmbed()
         .setColor(0xFFFF00)
-        .setTitle(`this avatar for ${message.mentions.users.first().username}:`)
-        .setImage(`${avatar + "?size=1024"}`)
-        .setFooter(`© ${customisation.ownername}`);
+        .setTitle(`${message.mentions.users.first().username}: avatar`)
+        .setImage(`${avatar}`)
+        .setFooter(`© OtakuGirl by ${customisation.ownername}`);
         message.channel.send({embed});
     } else {
-      const embed = new Discord.RichEmbed()
+      const embed = new Discord.MessageEmbed()
       .setColor(0xFFFF00)
-      .setTitle(`this avatar for ${message.author.username}:`)
-      .setImage(`${avatar + "?size=1024"}`)
-      .setFooter(`© ${customisation.ownername}`);
+      .setTitle(`${message.author.username}: avatar`)
+      .setImage(`${avatar + "?size=2048"}`)
+      .setFooter(`© OtakuGirl by ${customisation.ownername}`);
       message.channel.send({embed});
     }
 }

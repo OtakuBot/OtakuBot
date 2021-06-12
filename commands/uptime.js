22eb4eda12c742120b37438fd2c668b11413dde7
serin <oussamakatalonya1@gmail.com>
2021-06-12 17:39:21 +0100

    v3.3.2 | Update

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,6 +1,5 @@
-// By NightcoreAT#3678
-exports.run = (client, message) => { //From here our script will start
-    var milliseconds = parseInt((client.uptime % 1000) / 100), //This field is intended to give the bot the number of hours and timing it needs to know the length of time the bot is running on
+exports.run = (client, message) => {
+    var milliseconds = parseInt((client.uptime % 1000) / 100),
         seconds = parseInt((client.uptime / 1000) % 60),
         minutes = parseInt((client.uptime / (1000 * 60)) % 60),
         hours = parseInt((client.uptime / (1000 * 60 * 60)) % 24);
@@ -9,7 +8,7 @@ exports.run = (client, message) => { //From here our script will start
         minutes = (minutes < 10) ? "0" + minutes : minutes;
         seconds = (seconds < 10) ? "0" + seconds : seconds;
 
-        message.channel.send(":chart_with_upwards_trend: I've been running for** " + hours + " **hours, **" + minutes + "** minutes and **" + seconds + "." + milliseconds + "** seconds!"); //Here he will tell in full detail how long he spent while working
+        message.channel.send(":computer: UpWorking Sience** " + hours + " **hours, **" + minutes + "** minutes and **" + seconds + "." + milliseconds + "** seconds!");
 }
 
 exports.conf = {
@@ -20,8 +19,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: 'uptime', //follow description :)
-  description: 'Shows how long the bot has been online for.',
+  name: 'uptime',
+  description: ' ',
   usage: 'uptime'
 };
-// By NightcoreAT#3678

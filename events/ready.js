e1177869fc1d120c59c42c2ec9ee5857a34ae210
NightcoreAT <oussamakatalonya1@gmail.com>
2022-01-02 19:22:10 +0100

    remove chalk module
    we're not needed for chalk module afternow , that we've recorded a some issue at our bot folder

--- events/ready.js
+++ events/ready.js
@@ -1,18 +1,7 @@
-const chalk = require('chalk');
 const Discord = require('discord.js');
 const settings = require('../settings.json');
 const client = new Discord.Client();
 const fs = require('fs');
 module.exports = client => {
-    console.log(chalk.bgGreen.black(`your bot is serve [${client.guilds.cache.size}] : Servers`));
-  let blacklist = JSON.parse(fs.readFileSync("./blacklist.json", "utf8"));
-    client.guilds.cache.forEach((guild) => {
-      if (!blacklist[guild.ownerID]) {
-        return;
-      }else{
-        if(blacklist[guild.ownerID].state === true) {
-          message.guild.leave(guild.id)
-        }
-      }
-    })
+    console.log(`yourbots Online and ready to serve ${client.guilds.cache.size} servers.`);
 };
